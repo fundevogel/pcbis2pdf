6d50e5a9f048a172f2c36d0ac3298364e9d2fbbc
M. Folkers <hello@twobrain.io>
2019-05-12 01:14:52 +0200

    Reinstating proper example index.php

--- index.php
+++ index.php
@@ -2,18 +2,16 @@
 
 require_once('vendor/autoload.php');
 
-$object = new PCBIS2PDF\Providers\KNV;
+$object = new PCBIS2PDF\PCBIS2PDF;
 
 // Combines all CSV files from `src/csv/*.csv` to `src/Titelexport.csv`
 // $object->mergeCSV();
 
 // Do it like this ..
 try {
-    // $fromCSV = $object->CSV2PHP('./example/Titelexport.csv', ';');
-    $fromCSV = '978-3-522-18500-4';
-    $array = $object->getBook($fromCSV);
-    var_dump($array);
-    // $object->PHP2CSV($array);
+    $fromCSV = $object->CSV2PHP('./example/Titelexport.csv', ';');
+    $array = $object->processData($fromCSV);
+    $object->PHP2CSV($array);
 } catch (\Exception $e) {
     echo 'Error: ' . $e->getMessage(), "\n";
 }
